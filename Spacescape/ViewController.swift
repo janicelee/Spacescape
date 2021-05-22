b00834a6b071d7911eaef3ccc16594d6a0f27588
janicelee <j382lee@gmail.com>
2021-05-22 05:29:33 -0400

    Initial Commit

---
+++ Spacescape/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spacescape
+//
+//  Created by Janice Lee on 2021-05-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
